0c0939feb46126b3e9dd3e7556c4a27e051b68d8
Helis Freitas <dfhelis@gmail.com>
2024-10-05 14:49:28 -0300

    Initial commit

---
+++ src/app/components/app-navbar/app-navbar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './app-navbar.component.html',
+  styleUrl: './app-navbar.component.scss'
+})
+export class AppNavbarComponent {
+
+}
---
+++ src/app/components/app-root/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { AppHeaderComponent } from '../app-header/app-header.component';
+import { AppFooterComponent } from '../app-footer/app-footer.component';
+import { AppNavbarComponent } from '../app-navbar/app-navbar.component';
+import { AppLayoutComponent } from '../app-layout/app-layout.component';
+import { AppContentComponent } from '../app-content/app-content.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, AppHeaderComponent, AppFooterComponent, AppNavbarComponent, AppLayoutComponent, AppContentComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'my-delivery App';
+}
